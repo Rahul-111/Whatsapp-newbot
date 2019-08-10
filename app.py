833e1e6369a9d112829785b1c829cc350c60e120
Rahul-111 <rahulsri1997@gmail.com>
2019-08-10 17:56:06 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,53 @@
+# from flask import Flask, request
+# from twilio.twiml.messaging_response import MessagingResponse
+# import utils
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
+# @app.route("/sms", methods=['POST'])
+# def sms_reply():
+#     """Respond to incoming calls with a simple text message."""
+#     # Fetch the message
+#     print(request.form)
+#     msg = request.form.get('Body')
+#     sender = request.form.get('From')
+
+#     # Create reply
+#     resp = MessagingResponse()
+#     # resp.message("You said: {}".format(msg)).media("https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940")
+#     resp.message(utils.fetch_reply(msg,sender))
+#     return str(resp)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+#     
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
+from utils import fetch_reply
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    print(request.form)
+    msg = request.form.get('Body')
+    sender = request.form.get('From')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message(fetch_reply(msg, sender))
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
